6238fd923c3b669c0725f87f32fb7c867d428d97
root <root@mintdev.local>
2017-03-30 15:12:13 +0300

    v 1.1

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
 include("ozontravelparser.php");
  
-$startDate = "2017-03-30";
-$endDate = "2017-04-01";
+$startDate = "2017-04-01";
+$endDate = "2017-04-30";
 $interval = 1;
 $parser = new OzonTravelParser( $startDate, $endDate, $interval );
+$parser->extract_all_data();
+
+
+
+
+$parser->write_data(); 
+$parser->save_to_xls();
+
 
 echo "<pre>";
-var_dump ( $parser->extract_all_data() );
-$parser->write_data(); 
+var_dump ( $parser->get_log() );
+
--- ozontravelparser.php
+++ ozontravelparser.php
@@ -1,8 +1,8 @@
 <?php
- 
+
 class OzonTravelParser
 {
-    static $url      = "https://www.ozon.travel/hotel_by_accommodation/[[hotelId]]/in[[dateIn]]out[[dateOut]]/?Dlts=[[adults]]";
+    static $url       = "https://www.ozon.travel/hotel_by_accommodation/[[hotelId]]/in[[dateIn]]out[[dateOut]]/?Dlts=[[adults]]";
     private $hotelIds = [
         1  => [
             "name" => "Риксос",
@@ -45,18 +45,18 @@ class OzonTravelParser
             "id"   => '1691211287000/1628187573000',
         ],
     ];
-    private  $dateStart, $dateEnd, $interval;
+    private $dateStart, $dateEnd, $interval;
     private $log;
 
-    public function __construct($dateStart, $dateEnd = null, int $interval)
+    public function __construct($dateStart, $dateEnd = null,   $interval)
     {
-        include_once __DIR__.'/lib/html_parser/simple_html_dom.php';
-        
+        include_once __DIR__ . '/lib/html_parser/simple_html_dom.php';
+
         $this->set_dates($dateStart, $dateEnd, $interval);
         $this->log = [
-            "source"      => "ozon.travel",
-            "requestDate" => date("Y-m-d"),
-            "data"        => [],
+            "source"         => "ozon.travel",
+            "requestDate"    => date("Y-m-d"),
+            "data"           => [],
             "rooms_category" => [],
         ];
     }
@@ -68,10 +68,10 @@ class OzonTravelParser
 
     public function load($filename)
     {
-        $this->log          = json_decode(file_get_contents($filename));
-        $this->dateStart    = $this->log["requestDate"];
-        $this->dateEnd      = $this->log["requestDate"];
-        $this->interval     = 1;
+        $this->log       = json_decode(file_get_contents($filename));
+        $this->dateStart = $this->log["requestDate"];
+        $this->dateEnd   = $this->log["requestDate"];
+        $this->interval  = 1;
     }
 
     /*
@@ -80,11 +80,10 @@ class OzonTravelParser
 
     public function get_hotels()
     {
-         return $this->hotelIds;
+        return $this->hotelIds;
     }
 
-    
-    public function get_hotel_name(int $id)
+    public function get_hotel_name(  $id)
     {
         if (array_key_exists($id, $this->$hotelIds)) {
             return $this->hotelIds[$id]["name"];
@@ -93,7 +92,7 @@ class OzonTravelParser
         }
     }
 
-    public function set_dates($dateStart, $dateEnd = null, int $interval)
+    public function set_dates($dateStart, $dateEnd = null,   $interval)
     {
         //date format is 'Y-m-d'
         //TODO - if dateEnd is null, it must be dateStart + 1 day
@@ -103,98 +102,117 @@ class OzonTravelParser
         $this->interval  = $interval;
     }
 
-    public function extract_all_data( $test = false)
+    public function extract_all_data($test = false)
     {
         $dateStart = date_create_from_format('Y-m-d', $this->dateStart);
         $dateEnd   = date_create_from_format('Y-m-d', $this->dateEnd);
         $interval  = $this->interval;
 
         for ($i = clone ($dateStart); $i <= $dateEnd; $i->modify("+1 day")) {
-        	if (!array_key_exists($i->format('Y-m-d'), $this->log["data"])) $this->log["data"][$i->format('Y-m-d')] = [];
             $date2 = clone ($i);
             $date2->modify("+{$interval} day");
-            foreach ( $this->get_hotels() as $hotel ) {
-                if ( !array_key_exists($hotel["name"], $this->log["rooms_category"]) ) $this->log["rooms_category"][$hotel["name"]] = [];
-        	    $hotelId = $hotel["id"];
+            foreach ($this->get_hotels() as $hotel) {
+                $hotelId   = $hotel["id"];
                 $hotelName = $hotel["name"];
-	            for ($person=1;$person<3;$person++) {
-		            try {
-		                $url = str_replace(
-		                    array(
-		                        "[[hotelId]]",
-		                        "[[dateIn]]",
-		                        "[[dateOut]]",
-		                        "[[adults]]"
-		                    ),
-		                    array(
-		                        $hotelId,
-		                        $i->format('Y-m-d'),
-		                        $date2->format('Y-m-d'),
-		                        $person
-		                    ),
-		                    self::$url
-		                );
+                if (!array_key_exists($hotelName, $this->log["rooms_category"])) {
+                    $this->log["rooms_category"][$hotelName] = [];
+                }
+
+                if (!array_key_exists($hotelName, $this->log["data"])) {
+                    $this->log["data"][$hotelName] = [];
+                }
+
+                for ($person = 1; $person < 3; $person++) {
+
+                    try {
+                        $url = str_replace(
+                            array(
+                                "[[hotelId]]",
+                                "[[dateIn]]",
+                                "[[dateOut]]",
+                                "[[adults]]",
+                            ),
+                            array(
+                                $hotelId,
+                                $i->format('Y-m-d'),
+                                $date2->format('Y-m-d'),
+                                $person,
+                            ),
+                            self::$url
+                        );
                         if ($test) {
-                            echo $url."<BR>";
+                            echo $url . "<BR>";
                             continue;
-                            
+
                         }
 
-		                $path = __DIR__ . "/cache/" . md5($url) . date("Y-m-d") . ".cache";
-		                if (!is_file($path) || filesize($path) <= 0) {
-		                    file_put_contents($path, file_get_contents($url));
-		                }
-		                $html = file_get_html($path);
-		                if (!$html) {
-		                    continue;
-		                }
+                        $path = __DIR__ . "/cache/" . md5($url) . date("Y-m-d") . ".cache";
+                        if (!is_file($path) || filesize($path) <= 0) {
+                            file_put_contents($path, file_get_contents($url));
+                        }
+                        $html = file_get_html($path);
+                        if (!$html) {
 
-		                foreach ($html->find('#booking #rooms ul.rooms > li.room') as $room) {
-		                    $tariffs   = [];
-		                    $room_name = trim($room->find('span.room-name-inner', 0)->plaintext);
-                            if (strlen($room_name) <1) $room_name = "None";
-                            if ( !in_array($room_name, $this->log["rooms_category"][$hotelName] ) )
-                                array_push($this->log["rooms_category"][$hotelName], $room_name );
-                            $room_id = array_search($room_name,$this->log["rooms_category"][$hotelName] );
+                            continue;
+                        }
 
-		                    foreach ($room->find('ul.tariffs  li.tariff') as $tariff) {
-		                        $conditions = [];
-		                        foreach ($tariff->find("div.tariff-services li") as $t) {
-		                            $conditions[] = $t->plaintext;
-		                        }
-		                        $conditions[] = $room->find("div.tariff-cancel-conditions span.inner", 0)->plaintext;
-		                        $tariffs[]    = array(
+                        foreach ($html->find('#booking #rooms ul.rooms > li.room') as $room) {
+                            $tariffs   = [];
+                            $room_name = trim($room->find('div.room-name', 0)->plaintext);
+                            if (strlen($room_name) < 1) {
+                                $room_name = "None";
+                            }
 
-		                            "conditions" => implode(",", $conditions),
-		                            "price"      => trim($tariff->find("span.tariff-price-current-value", 0)->plaintext),
-		                        );
-		                    }
-		                    $this->log["data"][$i->format('Y-m-d')][] = [
-		                    	"hotel"	   => $hotelName,
-		                        "room"     => $room_name,
-                                "room_category_index" => $room_id,
-		                        
-		                        "capacity" => $person, 
-		                        "tariffs"  => $tariffs,
-		                    ];
-                           
-		                }
-		            } catch (Exception $e) {
+                            if (!array_key_exists($room_name, $this->log["data"][$hotelName])) {
+                                $this->log["data"][$hotelName][$room_name] = [];
+                            }
 
-		            }
+                            if (!array_key_exists($person, $this->log["data"][$hotelName][$room_name])) {
+                                $this->log["data"][$hotelName][$room_name][$person] = [];
+                            }
 
-		        }
-		    }
+                            if (!in_array($room_name, $this->log["rooms_category"][$hotelName])) {
+                                array_push($this->log["rooms_category"][$hotelName], $room_name);
+                            }
+
+                            $room_id = array_search($room_name, $this->log["rooms_category"][$hotelName]);
+
+                            foreach ($room->find('ul.tariffs  li.tariff') as $tariff) {
+                                $conditions = [];
+                                foreach ($tariff->find("div.tariff-services li") as $t) {
+                                    $conditions[] = $t->plaintext;
+                                }
+                                $conditions[] = $room->find("div.tariff-cancel-conditions span.inner", 0)->plaintext;
+                                $tariffs[]    = array(
+
+                                    "conditions" => trim(implode(",", $conditions)),
+                                    "price"      => trim($tariff->find("span.tariff-price-current-value", 0)->plaintext),
+
+                                );
+                            }
+                            $this->log["data"][$hotelName][$room_name][$person][$i->format("d-m-Y")] = $tariffs;
+
+                        }
+                    } catch (Exception $e) {
+
+                    }
+
+                }
+            }
         }
-        return $this->log;
-    } 
 
-     
+    }
+
+    public function get_log()
+    {
+        return $this->log;
+    }
+
     public function write_data()
     {
         $filename = sprintf(
             "ozon.travel_start%s_end%s_req%s.json",
-            
+
             $this->dateStart,
             $this->dateEnd,
             date("Y-m-d-His")
@@ -202,9 +220,96 @@ class OzonTravelParser
         file_put_contents($filename, json_encode($this->log));
     }
 
+    private function normalize(array &$array,   $size)
+    {
+        for ($i = 3; $i < $size; $i++) {
+            if (!array_key_exists($i, $array)) {
+                $array[$i] = " ";
+            }
+        }
+        ksort($array);
+    }
+
     public function save_to_xls()
     {
-        return null;
+        
+        include_once __DIR__ . '/lib/xls_writer/xlsxwriter.class.php';
+        ini_set('display_errors', 0);
+        ini_set('log_errors', 1);
+        error_reporting(E_ALL & ~E_NOTICE);
+        $filename = "res.xlsx";
+        header('Content-disposition: attachment; filename="' . XLSXWriter::sanitize_filename($filename) . '"');
+        header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        header('Content-Transfer-Encoding: binary');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        $writer = new XLSXWriter();
+        $writer->setAuthor('Ozon.travel vs. m.puzko');
+
+        $dateStart = date_create_from_format('Y-m-d', $this->dateStart);
+        $dateEnd   = date_create_from_format('Y-m-d', $this->dateEnd);
+        $interval  = $this->interval;
+
+        $header = [
+            0 => "Отель",
+            1 => "Номер",
+            2 => "Человек",
+        ];
+        //$dates = [];
+        for ($i = clone ($dateStart); $i <= $dateEnd; $i->modify("+1 day")) {
+            $header[] = $i->format("d-m-Y");
+            //$dates[]  = $i->format("d-m-Y");
+        }
+
+        $writer->writeSheetRow('Sheet1', $header); //sheet name here
+
+        foreach ($this->log["data"] as $hotel_name => $rooms_list) {
+            foreach ($rooms_list as $room_name => $persons) {
+                foreach ($persons as $person_qty => $dates) {
+
+                    $rows   = [];
+                    $rows[] = [
+                        0 => $hotel_name,
+                        1 => $room_name,
+                        2 => $person_qty,
+                    ];
+                    foreach ($dates as $date => $tariffs) {
+                        $date_index = array_search($date, $header);
+                        foreach ($tariffs as $tariff) {
+                            $inserted = 0;
+                            foreach ($rows as &$row) {
+                                if ( !array_key_exists($date_index, $row) && $inserted == 0 ) {
+                                    $row[$date_index] = $tariff["price"];
+                                    $inserted         = 1;
+                                }
+                            }
+
+                            if ( $inserted == 0 ) {
+                                $rows[] = [
+                                    0           => $hotel_name,
+                                    1           => $room_name,
+                                    2           => $person_qty,
+                                    $date_index => $tariff["price"],
+                                ];
+                            }
+ 
+                        }
+                    }
+                   
+                    foreach ($rows as &$row) {
+                        $this->normalize($row, count($header));
+                        $writer->writeSheetRow('Sheet1', $row);
+                    }
+
+                }
+            }
+
+        }
+
+ 
+        $writer->writeToStdOut();
+        exit(0);
+       
     }
 
 }
